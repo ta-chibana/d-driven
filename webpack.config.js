d5ae3ae14ce36149c1e3ea60cfe533b5c3ae5456
ta-chibana <tachibana.gmc@gmail.com>
2018-01-24 00:33:29 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    index: './src/index.jsx'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node-modules/
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      Actions: path.resolve(__dirname, 'src/actions'),
+      Components: path.resolve(__dirname, 'src/components'),
+      Containers: path.resolve(__dirname, 'src/containers'),
+      Reducers: path.resolve(__dirname, 'src/reducers'),
+      Sagas: path.resolve(__dirname, 'src/sagas')
+    }
+  },
+  devtool: 'inline-source-map'
+}
